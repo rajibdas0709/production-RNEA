958678a60c14f564decea7e25ee674401f2908e2
rajibdas0709 <163306213+rajibdas0709@users.noreply.github.com>
2024-03-14 12:07:04 +0530

    Update sendMailHelper.js

--- helpers/sendMailHelper.js
+++ helpers/sendMailHelper.js
@@ -16,9 +16,7 @@ const sendVerificationEmail = async (email, verificationToken) => {
         from: 'React Native E-commerce App',
         to: email,
         subject: "Email Verification",
-        text: `Please click the following link to verify your email : https://production-rnea-server.onrender.com
-
-/api/v1/auth/verify/${verificationToken}`
+        text: `Please click the following link to verify your email : https://production-rnea-server.onrender.com/api/v1/auth/verify/${verificationToken}`
     }
 
     //send the email 
